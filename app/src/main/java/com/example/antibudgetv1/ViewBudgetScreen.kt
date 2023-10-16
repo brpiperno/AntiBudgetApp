8ba2235e6891ed9666cc500a740f1cd7935cd8a3
Ben Piperno <BRPiperno@gmail.com>
2023-10-15 22:51:58 -0400

    Created basic budget view with sticky headers

--- app/src/main/java/com/example/antibudgetv1/ViewBudgetScreen.kt
+++ app/src/main/java/com/example/antibudgetv1/ViewBudgetScreen.kt
@@ -19,10 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.antibudgetv1.data.discoverCredit
 import com.example.antibudgetv1.data.discoverCreditTransactions
 import com.example.antibudgetv1.data.testBudget
-import com.example.antibudgetv1.model.IAccount
 import com.example.antibudgetv1.model.IAntiBudget
 import com.example.antibudgetv1.model.ITransaction
 
